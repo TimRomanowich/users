2caedc8c7d392ad95b33531f2c979162a1bd6d79
TimRomanowich <romanowt@oregonstate.edu>
2024-07-13 09:06:00 -0700

    initial commit

--- user_mgmt/user_mgmt/urls.py
+++ user_mgmt/user_mgmt/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for users project.
+URL configuration for user_mgmt project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
